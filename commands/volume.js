31079db024f8da27c29074351410f7324bb44791
Luzivog <thpfai@gmail.com>
2020-11-16 15:01:52 +0100

    Enters*

--- commands/volume.js
+++ commands/volume.js
@@ -6,10 +6,10 @@ module.exports.run = async (client, message, args) => {
     if(args.length > 1) {return message.channel.send(strings.toMuchArgsVolume);};
 
     message.channel.send(strings.volumeChanged.replace("VOLUME", args[0]));
+    
     return queue.get("queue").connection.dispatcher.setVolumeLogarithmic(args[0] / 5);
 
 };
-
 
 module.exports.names = {
     list: ["volume", "v"]
